4746fd28517966bf78c7f7ed2cf6cca45019962c
searayeah <insslava@gmail.com>
2023-05-21 23:29:45 +0300

    4.3

--- main.py
+++ main.py
@@ -3,14 +3,31 @@ import os
 import re
 import struct
 import time
-import traceback
+import logging
 import winreg
 import vdf
 import requests
 
+# logging
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+log_level = {
+    "DEBUG": logging.DEBUG,
+    "INFO": logging.INFO,
+    "WARNING": logging.WARNING,
+    "ERROR": logging.ERROR,
+    "CRITICAL": logging.CRITICAL,
+}
+logger.info("Version: 4.3")
+
+# main globals
 DOTA_APP_ID = "570"
-DEFAULT_HEX_STRING = "00 00 00 00 00 00 2E 40 00 00 96 44 00 00 61 45"  # 7.32e
-SERVER_HEX_STRING_LINK = "https://raw.githubusercontent.com/searayeah/dota-camera-distance/main/current_hex_string"
+DEFAULT_HEX_STRING = "00 00 00 00 00 00 2E 40 00 00 96 44 00 00 61 45"  # old 7.32e
+SERVER_HEX_STRING_LINK = (
+    "https://raw.githubusercontent.com/"
+    "searayeah/dota-camera-distance/main/current_hex_string"
+)
 DEFAULT_DISTANCE = "1200"
 STEAM_REGISTRY_KEY = os.path.join("SOFTWARE", "WOW6432Node", "Valve", "Steam")
 CLIENT_DLL_PATH = os.path.join(
@@ -18,10 +35,7 @@ CLIENT_DLL_PATH = os.path.join(
 )
 LIBRARY_FOLDERS_PATH = os.path.join("steamapps", "libraryfolders.vdf")
 APP_MANIFEST_PATH = os.path.join("steamapps", f"appmanifest_{DOTA_APP_ID}.acf")
-
-DISTANCE_MESSAGE = "Enter distance[default 1200, recommended 1400]: "
-OLD_CONFIG_MESSAGE = "Detected old config style, rewriting..."
-CONFIG_ERROR_MESSAGE = "Something wrong with the config, creating new one"
+DOTA_URL = f"steam://rungameid/{DOTA_APP_ID}"
 
 
 def set_distance(hex_string, distance, client_dll_path):
@@ -31,6 +45,22 @@ def set_distance(hex_string, distance, client_dll_path):
     # Therefore, we use big strings e.g 00 00 00 00 00 00 2E 40 00 00 96 44 00 00 E1 44.
 
     hex_string = hex_string.lower().replace(" ", "")
+    hex_string_length = len(hex_string)
+
+    if hex_string_length <= 8:
+        raise Exception(
+            f"Hex string {hex_string} is only {hex_string_length} symbols long,"
+            " which makes search inaccurate, as there is certainly"
+            " more than one occurences of that string in client.dll file."
+            " Please update hex code to have at least 24 symbols."
+        )
+    elif hex_string_length <= 16:
+        logging.critical(
+            f"Hex string {hex_string} is only {hex_string_length} symbols long"
+            " which makes search inaccurate, as there might be"
+            " more than one occurences of that string in client.dll file."
+            " Please update hex code to have at least 24 symbols."
+        )
 
     # Converting DEFAULT_DISTANCE of 1200 to hex format.
     default_distance_hex = struct.pack("f", float(DEFAULT_DISTANCE)).hex()
@@ -50,12 +80,25 @@ def set_distance(hex_string, distance, client_dll_path):
     # and this allows to find and change distance regardless of its current value.
     distance_index = hex_string.find(default_distance_hex)
 
+    if distance_index == -1:
+        raise Exception(
+            f"Default hex distance {default_distance_hex}"
+            f" is not found in the hex string {hex_string}"
+        )
+    elif distance_index == 0:
+        logging.warning(
+            f"Hex string {hex_string} starting with default distance"
+            f" code {default_distance_hex} makes search a lot slower."
+            " Please shift the code, so it starts with other symbols"
+        )
+
     # Constructing regular expression to find the position in client.dll file.
     hex_string_regex = re.compile(
         hex_string[:distance_index]
         + f"\w{{{distance_hex_length}}}"  # regex \w{8} means any 8 characters [a-zA-Z0-9_]
         + hex_string[distance_index + distance_hex_length :]
     )
+    logger.debug(f"Regex code {hex_string_regex}")
 
     # Constructing string that would be used for replacement.
     distance_hex_string = (
@@ -63,28 +106,48 @@ def set_distance(hex_string, distance, client_dll_path):
         + distance_hex
         + hex_string[distance_index + distance_hex_length :]
     )
+    logger.debug(f"Replacement string {distance_hex_string}")
 
     with open(client_dll_path, "rb") as f:
         client_dll_hex = f.read().hex()
+        logger.debug("client.dll file read")
+
+    matches = re.findall(hex_string_regex, client_dll_hex)
+    matches_count = len(matches)
+    logging.debug(f"Matches count: {matches_count}. Matches: {matches}")
+
+    if matches_count == 0:
+        # If there are no matches, that means that current hex string is not present in
+        # client.dll file and needs to be updated.
+        # This usually happens when Valve release big updates to the game.
+        raise Exception(
+            "Couldn't find the hex value in client.dll file."
+            " Valve might have changed it."
+        )
+    elif matches_count > 1:
+        raise Exception(
+            f"Hex string {hex_string} is not precise enough to clearly find"
+            f" it's location in client.dll file. Currently found {matches_count}"
+            " matches. Please, update the string's length to be more precise."
+        )
 
     # Replacing
     client_dll_hex_new, changes_count = re.subn(
         hex_string_regex, distance_hex_string, client_dll_hex, 1
     )
 
-    # If there are no changes, that means that current hex string is not present in
-    # client.dll file and needs to be updated.
-    # This usually happens when Valve release big updates to the game.
-    if changes_count == 0:
+    new_matches = re.findall(hex_string_regex, client_dll_hex_new)
+    logger.info(f"Old: {matches}")
+    logger.info(f"New: {new_matches}")
+
+    try:
+        with open(client_dll_path, "wb") as f:
+            f.write(bytes.fromhex(client_dll_hex_new))
+            logger.debug("client.dll file written")
+    except PermissionError as e:
         raise Exception(
-            "Couldn't find the hex value in client.dll file. Valve might have changed it."
+            "Couldn't open client.dll file, close Dota 2 before launching the app",
         )
-
-    print(f"Old: {re.search(hex_string_regex, client_dll_hex).group()}")
-    print(f"New: {re.search(hex_string_regex, client_dll_hex_new).group()}")
-
-    with open(client_dll_path, "wb") as f:
-        f.write(bytes.fromhex(client_dll_hex_new))
 
 
 def get_steam_path():
@@ -93,6 +156,7 @@ def get_steam_path():
     hkey = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, STEAM_REGISTRY_KEY)
     steam_path = winreg.QueryValueEx(hkey, "InstallPath")[0]
     winreg.CloseKey(hkey)
+    logger.debug(f"Retrieved Steam path {steam_path} from winreg")
     return steam_path
 
 
@@ -102,9 +166,18 @@ def get_steam_library_path(steam_path):
     # Dota and Steam can have different install locations.
     library_folders_path = os.path.join(steam_path, LIBRARY_FOLDERS_PATH)
     library_folders = vdf.load(open(library_folders_path))["libraryfolders"]
+    logger.debug(f"Opened {library_folders_path}")
     for key in library_folders:
         if DOTA_APP_ID in library_folders[key]["apps"]:
-            return library_folders[key]["path"]
+            dota_path = library_folders[key]["path"]
+            logger.debug(f"Found Dota path in library folders {dota_path}")
+            return dota_path
+    raise Exception(
+        "Dota 2 path was not found in libraryfolders.vdf file."
+        " This usually happens when Dota 2 is not installed on the system."
+        " If you have just installed Dota 2 you need to restart Steam"
+        " for it to apply the changes to specific files."
+    )
 
 
 def dota_was_updating(steam_library_path):
@@ -114,13 +187,13 @@ def dota_was_updating(steam_library_path):
     app_manifest_path = os.path.join(steam_library_path, APP_MANIFEST_PATH)
     app_manifest = vdf.load(open(app_manifest_path))
     app_status = app_manifest["AppState"]["StateFlags"]
+    logger.debug(f"Opened app manifest, initial status: {app_status}")
     if app_status != "4":
         while app_status != "4":
-            print(f"Waiting for Dota 2 to get updates, status: {app_status}", end="\r")
+            logger.info(f"Waiting for Dota 2 to get updates, status: {app_status}")
             time.sleep(1)
             app_manifest = vdf.load(open(app_manifest_path))
             app_status = app_manifest["AppState"]["StateFlags"]
-        print()
         return True
     else:
         return False
@@ -132,11 +205,11 @@ def get_current_hex_string():
     try:
         response = requests.get(SERVER_HEX_STRING_LINK)
         response.raise_for_status()
-        print("String received from GitHub")
+        response_text_repr = repr(response.text)
+        logger.debug(f"String {response_text_repr} received from GitHub")
         return response.text.strip()
     except requests.exceptions.RequestException as e:
-        print(e)
-        print("Couldn't receive string from GitHub, using the default one")
+        logger.error("Couldn't receive string from GitHub, using the default one")
         return DEFAULT_HEX_STRING
 
 
@@ -147,12 +220,13 @@ def set_config():
     try:
         config_file.read(config_path)
     except Exception as e:
-        print(e)
-        print(CONFIG_ERROR_MESSAGE)
+        logger.error(
+            "Something wrong with the config, creating new one...", exc_info=True
+        )
 
     if config_file.has_section("DOTA-CAMERA-DISTANCE"):
         config_file.clear()
-        print(OLD_CONFIG_MESSAGE)
+        logger.info("Detected old config style, rewriting...")
 
     if not config_file.has_section("CAMERA-DISTANCE"):
         config_file["CAMERA-DISTANCE"] = {}
@@ -161,13 +235,19 @@ def set_config():
     camera_config = config_file["CAMERA-DISTANCE"]
     path_config = config_file["PATHS"]
 
+    if not camera_config.get("logging_level"):
+        camera_config["logging_level"] = "INFO"
+    logger.setLevel(log_level.get(camera_config["logging_level"], logging.INFO))
+    logger.info(f"Logging level: {logging.getLevelName(logger.getEffectiveLevel())}")
+
     if not camera_config.get("distance"):
-        camera_config["distance"] = input(DISTANCE_MESSAGE) or DEFAULT_DISTANCE
-    print(f"Distance: {camera_config['distance']}")
+        distance_message = "Enter distance[default 1200, recommended 1400]: "
+        camera_config["distance"] = input(distance_message) or DEFAULT_DISTANCE
+    logger.info(f"Distance: {camera_config['distance']}")
 
     if not camera_config.get("receive_hex_from_git"):
         camera_config["receive_hex_from_git"] = "yes"
-    print(f"Receive hex from git: {camera_config['receive_hex_from_git']}")
+    logger.info(f"Receive hex from git: {camera_config['receive_hex_from_git']}")
 
     # I will update the string through github current_hex_string file
     # but if you obtained the new string faster than me, you can
@@ -177,70 +257,68 @@ def set_config():
         "hex_string"
     ):
         camera_config["hex_string"] = get_current_hex_string()
-    print(f"Hex string: {camera_config['hex_string']}")
+    logger.info(f"Hex string: {camera_config['hex_string']}")
+
+    if not camera_config.get("autostart_game"):
+        camera_config["autostart_game"] = "yes"
+    logger.info(f"Autostart game: {camera_config['autostart_game']}")
 
     if not path_config.get("steam_path"):
         path_config["steam_path"] = get_steam_path()
-    print(f"Steam path: {path_config['steam_path']}")
+    logger.info(f"Steam path: {path_config['steam_path']}")
 
     if not path_config.get("steam_library_path"):
         path_config["steam_library_path"] = get_steam_library_path(
             path_config["steam_path"]
         )
-    print(f"Steam library path: {path_config['steam_library_path']}")
+    logger.info(f"Steam library path: {path_config['steam_library_path']}")
 
     if not path_config.get("client_dll_path"):
         path_config["client_dll_path"] = os.path.join(
             path_config["steam_library_path"], CLIENT_DLL_PATH
         )
-    print(f"Client.dll path: {path_config['client_dll_path']}")
+    logger.info(f"client.dll path: {path_config['client_dll_path']}")
 
     with open(config_path, "w") as configfile:
         config_file.write(configfile)
+        logger.info(f"Updated config: {config_path}")
 
-    print(f"Updated {config_path}")
-
-    return (
-        camera_config["distance"],
-        camera_config["receive_hex_from_git"],
-        camera_config["hex_string"],
-        path_config["steam_path"],
-        path_config["steam_library_path"],
-        path_config["client_dll_path"],
-    )
+    return camera_config, path_config
 
 
 def main():
-    (
-        distance,
-        receive_hex_from_git,
-        hex_string,
-        steam_path,
-        steam_library_path,
-        client_dll_path,
-    ) = set_config()
+    camera_config, path_config = set_config()
 
-    set_distance(hex_string, distance, client_dll_path)
-    os.startfile(f"steam://rungameid/{DOTA_APP_ID}")  # Windows only
-    print("Launching Dota 2 ...")
+    set_distance(
+        camera_config["hex_string"],
+        camera_config["distance"],
+        path_config["client_dll_path"],
+    )
+    if camera_config.getboolean("autostart_game"):
+        os.startfile(DOTA_URL)  # Windows only
+        logging.info("Launching Dota 2 ...")
 
-    # When launching Dota for the first time it might get updates,
-    # so client.dll needs to be rewritten again.
-    if dota_was_updating(steam_library_path):
-        set_distance(hex_string, distance, client_dll_path)
-        print('Press "Play game"')
+        # When launching Dota for the first time it might get updates,
+        # so client.dll needs to be rewritten again.
+        if dota_was_updating(path_config["steam_library_path"]):
+            set_distance(
+                camera_config["hex_string"],
+                camera_config["distance"],
+                path_config["client_dll_path"],
+            )
+            logging.info('Press "Play game"')
 
 
 if __name__ == "__main__":
     try:
         main()
-    except Exception:
-        traceback.print_exc()
-        print(
-            "Program crashed, send me a screenshot via "
-            + "https://github.com/searayeah/dota-camera-distance/issues"
+    except Exception as e:
+        logger.error("Program crashed", exc_info=True)
+        logger.error(
+            "If nothing helps or there's a bug, send me a screenshot via"
+            " https://github.com/searayeah/dota-camera-distance/issues",
         )
     finally:
         for i in range(5, 0, -1):
-            print(f"Exit in: {i}", end="\r")
+            logger.info(f"Exit in: {i}")
             time.sleep(1)
