5f66e71bb24a685b05cd7f04a3048990f7648491
searayeah <insslava@gmail.com>
2023-08-10 20:28:51 +0300

    First version on linux support

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import re
 import struct
 import time
 import logging
-import winreg
 import vdf
 import requests
+import sys
 
 # logging
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
@@ -19,26 +19,81 @@ log_level = {
     "ERROR": logging.ERROR,
     "CRITICAL": logging.CRITICAL,
 }
-logger.info("Version: 4.3")
+logger.info(f"OS: {sys.platform}")
+logger.info("Version: 5.0")
 
-# main globals
+# globals
 DOTA_APP_ID = "570"
-DEFAULT_HEX_STRING = "00 00 AE 42 00 00 96 44 00 00 C8 44 00 40 9C 45"  # 7.33c
-SERVER_HEX_STRING_LINK = (
-    "https://raw.githubusercontent.com/"
-    "searayeah/dota-camera-distance/main/current_hex_string"
-)
 DEFAULT_DISTANCE = "1200"
-STEAM_REGISTRY_KEY = os.path.join("SOFTWARE", "WOW6432Node", "Valve", "Steam")
-CLIENT_DLL_PATH = os.path.join(
-    "steamapps", "common", "dota 2 beta", "game", "dota", "bin", "win64", "client.dll"
-)
+
 LIBRARY_FOLDERS_PATH = os.path.join("steamapps", "libraryfolders.vdf")
 APP_MANIFEST_PATH = os.path.join("steamapps", f"appmanifest_{DOTA_APP_ID}.acf")
-DOTA_URL = f"steam://rungameid/{DOTA_APP_ID}"
+
+if sys.platform.startswith("win32"):
+    import winreg
+
+    DEFAULT_HEX_STRING = "00 00 AE 42 00 00 96 44 00 00 C8 44 00 40 9C 45"
+    SERVER_HEX_STRING_LINK = (
+        "https://raw.githubusercontent.com/"
+        "searayeah/dota-camera-distance/main/current_hex_string"
+    )
+
+    STEAM_REGISTRY_KEY = os.path.join("SOFTWARE", "WOW6432Node", "Valve", "Steam")
+    SHARED_LIBRARY_PATH = os.path.join(
+        "steamapps",
+        "common",
+        "dota 2 beta",
+        "game",
+        "dota",
+        "bin",
+        "win64",
+        "client.dll",
+    )
+
+    DOTA_URL = f"steam://rungameid/{DOTA_APP_ID}"
+
+    def get_steam_path():
+        # Getting Steam path from Windows Registry.
+        # This does not guarantee finding Dota 2 folder.
+        hkey = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, STEAM_REGISTRY_KEY)
+        steam_path = winreg.QueryValueEx(hkey, "InstallPath")[0]
+        winreg.CloseKey(hkey)
+        logger.debug(
+            f"Retrieved Steam path: {steam_path} from winreg: {STEAM_REGISTRY_KEY}"
+        )
+        return steam_path
+
+elif sys.platform.startswith("linux"):
+    DEFAULT_HEX_STRING = "00 00 AF 43 00 80 3B 44 00 00 96 44 33 33 33 3F"
+    SERVER_HEX_STRING_LINK = (
+        "https://raw.githubusercontent.com/"
+        "searayeah/dota-camera-distance/main/current_hex_string_linux"
+    )
+    SHARED_LIBRARY_PATH = os.path.join(
+        "steamapps",
+        "common",
+        "dota 2 beta",
+        "game",
+        "dota",
+        "bin",
+        "linuxsteamrt64",
+        "libclient.so",
+    )
+
+    DOTA_URL = f"steam steam://rungameid/{DOTA_APP_ID}"
+
+    def get_steam_path():
+        steam_path = os.path.expanduser(os.path.join("~", ".steam", "steam"))
+        logger.debug(f"Retrieved Steam path: {steam_path} (linux)")
+        return steam_path
+
+elif sys.platform.startswith("darwin"):
+    raise Exception("OS not supported")
+else:
+    raise Exception("OS not supported")
 
 
-def set_distance(hex_string, distance, client_dll_path):
+def set_distance(hex_string, distance, shared_library_path):
     # First of all, it is important to notice that distance value is only
     # 8 numbers long, but 8 numbers is not enough to determine its exact location,
     # as there can be more than one occurence of the sequence.
@@ -51,14 +106,14 @@ def set_distance(hex_string, distance, client_dll_path):
         raise Exception(
             f"Hex string {hex_string} is only {hex_string_length} symbols long,"
             " which makes search inaccurate, as there is certainly"
-            " more than one occurences of that string in client.dll file."
+            " more than one occurences of that string in library file."
             " Please update hex code to have at least 24 symbols."
         )
     elif hex_string_length <= 16:
         logger.critical(
             f"Hex string {hex_string} is only {hex_string_length} symbols long"
             " which makes search inaccurate, as there might be"
-            " more than one occurences of that string in client.dll file."
+            " more than one occurences of that string in library file."
             " Please update hex code to have at least 24 symbols."
         )
 
@@ -92,7 +147,7 @@ def set_distance(hex_string, distance, client_dll_path):
             " Please shift the code, so it starts with other symbols"
         )
 
-    # Constructing regular expression to find the position in client.dll file.
+    # Constructing regular expression to find the position in shared library file.
     hex_string_regex = re.compile(
         hex_string[:distance_index]
         + f"\w{{{distance_hex_length}}}"  # regex \w{8} means any 8 characters [a-zA-Z0-9_]
@@ -108,59 +163,47 @@ def set_distance(hex_string, distance, client_dll_path):
     )
     logger.debug(f"Replacement string: {distance_hex_string}")
 
-    with open(client_dll_path, "rb") as f:
-        client_dll_hex = f.read().hex()
-    logger.debug(f"Read: {client_dll_path}")
+    with open(shared_library_path, "rb") as f:
+        shared_library_hex = f.read().hex()
+    logger.debug(f"Read: {shared_library_path}")
 
-    matches = re.findall(hex_string_regex, client_dll_hex)
+    matches = re.findall(hex_string_regex, shared_library_hex)
     matches_count = len(matches)
     logger.debug(f"Matches count: {matches_count}. Matches: {matches}")
 
     if matches_count == 0:
         # If there are no matches, that means that current hex string is not present in
-        # client.dll file and needs to be updated.
+        # shared library file and needs to be updated.
         # This usually happens when Valve release big updates to the game.
         raise Exception(
-            "Couldn't find the hex value in client.dll file."
+            "Couldn't find the hex value in library file."
             " Valve might have changed it."
         )
     elif matches_count > 1:
         raise Exception(
             f"Hex string {hex_string} is not precise enough to clearly find"
-            f" it's location in client.dll file. Currently found {matches_count}"
+            f" it's location in library file. Currently found {matches_count}"
             " matches. Please, update the string's length to be more precise."
         )
 
     # Replacing
-    client_dll_hex_new, changes_count = re.subn(
-        hex_string_regex, distance_hex_string, client_dll_hex, 1
+    shared_library_hex_new, changes_count = re.subn(
+        hex_string_regex, distance_hex_string, shared_library_hex, 1
     )
     logger.debug(f"Replaced string, changes count: {changes_count}")
 
-    new_matches = re.findall(hex_string_regex, client_dll_hex_new)
+    new_matches = re.findall(hex_string_regex, shared_library_hex_new)
     logger.debug(f"Old: {matches}")
     logger.debug(f"New: {new_matches}")
 
     try:
-        with open(client_dll_path, "wb") as f:
-            f.write(bytes.fromhex(client_dll_hex_new))
-        logger.debug(f"Written: {client_dll_path}")
+        with open(shared_library_path, "wb") as f:
+            f.write(bytes.fromhex(shared_library_hex_new))
+        logger.debug(f"Written: {shared_library_path}")
     except PermissionError as e:
         raise Exception(
-            "Couldn't open client.dll file, close Dota 2 before launching the app",
+            "Couldn't open shared library file, close Dota 2 before launching the app",
         )
-
-
-def get_steam_path():
-    # Getting Steam path from Windows Registry.
-    # This does not guarantee finding Dota 2 folder.
-    hkey = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, STEAM_REGISTRY_KEY)
-    steam_path = winreg.QueryValueEx(hkey, "InstallPath")[0]
-    winreg.CloseKey(hkey)
-    logger.debug(
-        f"Retrieved Steam path: {steam_path} from winreg: {STEAM_REGISTRY_KEY}"
-    )
-    return steam_path
 
 
 def get_steam_library_path(steam_path):
@@ -276,11 +319,11 @@ def set_config():
         )
     logger.info(f"Steam library path: {path_config['steam_library_path']}")
 
-    if not path_config.get("client_dll_path"):
-        path_config["client_dll_path"] = os.path.join(
-            path_config["steam_library_path"], CLIENT_DLL_PATH
+    if not path_config.get("shared_library_path"):
+        path_config["shared_library_path"] = os.path.join(
+            path_config["steam_library_path"], SHARED_LIBRARY_PATH
         )
-    logger.info(f"client.dll path: {path_config['client_dll_path']}")
+    logger.info(f"shared library path: {path_config['shared_library_path']}")
 
     with open(config_path, "w") as configfile:
         config_file.write(configfile)
@@ -295,19 +338,21 @@ def main():
     set_distance(
         camera_config["hex_string"],
         camera_config["distance"],
-        path_config["client_dll_path"],
+        path_config["shared_library_path"],
     )
     if camera_config.getboolean("autostart_game"):
-        os.startfile(DOTA_URL)  # Windows only
+        # os.startfile(DOTA_URL)  # Windows only
+        os.system("steam steam://rungameid/570")
+
         logger.info("Launching Dota 2 ...")
 
         # When launching Dota for the first time it might get updates,
-        # so client.dll needs to be rewritten again.
+        # so library file needs to be rewritten again.
         if dota_was_updating(path_config["steam_library_path"]):
             set_distance(
                 camera_config["hex_string"],
                 camera_config["distance"],
-                path_config["client_dll_path"],
+                path_config["shared_library_path"],
             )
             logger.info('Press "Play game"')
 
